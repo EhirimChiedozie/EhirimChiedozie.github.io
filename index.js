281b70f1af7b8156fbd6a5ce953fd9c01018c186
unknown <chiedoziedavidehirim@gmail.com>
2023-12-31 17:39:36 +0100

    Removed the javascript file

--- index.js
+++
@@ -1,9 +0,0 @@
-function addImageAttributes(){
-    let image = document.querySelectorAll('img')
-    for(var i=0; i<image.length; i++){
-        image[i].style.width = '200px'
-        image[i].style.height = '200px'
-    }
-}
-
-addImageAttributes()
